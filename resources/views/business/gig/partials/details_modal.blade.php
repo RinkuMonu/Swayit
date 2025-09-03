5bae3d47945564b35ee3b8e2d0965846d40f7ac6
nikichaturvedi <chaturvedinikita657@gmail.com>
2025-09-03 16:00:15 +0530

    Remove invalid Windows path file

--- resources/views/business/gig/partials/details_modal.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <h4>{{ $gig->title }}</h4>
-    <p><strong>Price:</strong> ${{ $gig->price }}</p>
-    <p><strong>Description:</strong> {!! $gig->description !!}</p>
-    <p><strong>Industry:</strong> {{ $gig->industry->name ?? 'N/A' }}</p>
-   
-</div>
--- routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\InfluencerController;
-
-
-Route::get('/live-influencer-search', [InfluencerController::class, 'fetchInfluencers']);
-Route::post('/notify-influencers', [InfluencerController::class, 'notifyInfluencers']);
-
